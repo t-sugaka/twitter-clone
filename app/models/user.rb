e4fe570c71ed11b461d418874c9fd843ca5a66ab
t-sugaka <sugaka.job@gmail.com>
2020-04-20 17:16:56 +0900

    アカウント作成時に名前を入力するよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :email, presence: true
+  validates :name, presence: true
 end
